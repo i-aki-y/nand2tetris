8a21800792f2d4a548490c8c519bda3a1e4ec29f
akiyuki.i <aki.y.ishikawa@gmail.com>
2020-05-30 12:51:07 +0900

    add: impl fill asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,61 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+	@KBD
+	D=M
+
+	@ON
+	D;JGT
+
+	@OFF
+	0;JMP
+
+(ON)
+	@i
+	M=0
+(FILL)
+	@i
+	D=M
+	@SCREEN
+	A=D+A
+	M=-1
+	@i
+	M=M+1
+	D=M
+	@8192
+	D=D-A
+	@LOOP
+	D;JGE
+	@FILL
+	0;JMP
+
+(OFF)
+	@i
+	M=0
+(CLR)
+	@i
+	D=M
+	@SCREEN
+	A=D+A
+	M=0
+	@i
+	M=M+1
+	D=M
+	@8192
+	D=D-A
+	@LOOP
+	D;JGE
+	@CLR
+	0;JMP
+(END)
+
+	@END
+	0;JMP
